a06ea5bf7a5e335e1c92b4f727a5c7438010931f
akreulach <akreulach@gmail.com>
2021-10-19 23:12:40 -0400

    Possible error in grayscaling

--- source.fs
+++ source.fs
@@ -2,5 +2,10 @@
     out vec4 FragColor;
     in vec3 ourColor;
     void main(){
-       FragColor = vec4(ourColor, 1.0f);
+        float near = 0.1;
+        float far = 20.0;
+        float z = gl_FragCoord.z * 2.0 - 1.0;
+        z = ((2.0 * near * far)/(far + near - z * (far - near))) / far;
+        //FragColor = vec4(ourColor, 1.0f);
+        FragColor = vec4(vec3(z), 1.0f);
     }
