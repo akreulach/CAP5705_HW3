a0e462f2def7333d6b9960a534371b507aabc765
akreulach <akreulach@gmail.com>
2021-10-29 18:20:55 -0400

    Final commit

--- README.md
+++ README.md
@@ -1,8 +1,24 @@
-This code prints a rainbow cube.
-
-It requires the modified version of cube.obj that is included in this zip.
+This code prints a rainbow cube, or a rainbow dragon skull. Can also show various lighting solutions.
 
 The code is both compiled and run by build.sh
     ./build.sh
     sh build.sh
 Both above commands should compile, run, and clean up after this program.
+
+It requires the modified version of cube.obj that is included in this zip.
+The unmodified dragon.obj included in this zip works as well.
+
+In order to change the object shown you must modify the file name on line 93 of assignment3.cpp
+    To show cube:
+        Do not modify
+    To show dragon skull:
+        Replace line 93 with
+            FILE * file = fopen("dragon.obj", "r");
+
+When the code is run, there are three ways to interact with the image. 
+    The UP and DOWN arrow keys will scale the object shown
+    The MOUSE can rotate the object
+    The Z key will show true color
+    The X key will show z-depth in grayscale
+    The C key will show Phong shading
+    The V key will show Gouraud shading
--- assignment3.cpp
+++ assignment3.cpp
@@ -131,18 +131,21 @@ int main()
             temp_normals.push_back(normal[2]);
         } else if(strcmp(lineHeader, "f") == 0){
             std::string vertex1, vertex2, vertex3;
-            unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
-            int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
-            if (matches != 9) {
+            //unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
+            unsigned int vertexIndex[3], normalIndex[3];
+            //int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
+            int matches = fscanf(file, "%d//%d %d//%d %d//%d\n", &vertexIndex[0], &normalIndex[0], &vertexIndex[1], &normalIndex[1], &vertexIndex[2], &normalIndex[2]);
+            if (matches != 6) {
                 printf("File can't be read by my dumb parser\n");
+                printf("%d",matches);
                 return false;
             }
             vertexIndices.push_back(vertexIndex[0]-1);
             vertexIndices.push_back(vertexIndex[1]-1);
             vertexIndices.push_back(vertexIndex[2]-1);
-            uvIndices    .push_back(uvIndex[0]-1);
-            uvIndices    .push_back(uvIndex[1]-1);
-            uvIndices    .push_back(uvIndex[2]-1);
+            //uvIndices    .push_back(uvIndex[0]-1);
+            //uvIndices    .push_back(uvIndex[1]-1);
+            //uvIndices    .push_back(uvIndex[2]-1);
             normalIndices.push_back(normalIndex[0]-1);
             normalIndices.push_back(normalIndex[1]-1);
             normalIndices.push_back(normalIndex[2]-1);
